1db7a153fb988614161859728879cc0eaeb8b63b
Vincent Berenz <vincentberenz@tuebingen.mpg.de>
2016-03-21 12:01:02 +0100

    copied yaml_eigen.h from humanoid_control/io_tools

--- ci_example_python/src/ci_example/pid.py
+++ ci_example_python/src/ci_example/pid.py
@@ -4,17 +4,22 @@ import os
 ### configuration for getting default, ros or file based configuration
 
 class Default_configuration:
+
     kp=1
     kd=1
     ki=1
 
+
 class ROS_configuration:
+
     ROSPARAM_KP = "gains_kp"
     ROSPARAM_KD = "gains_kd"
     ROSPARAM_KI = "gains_ki"
 
+
 class Config_file_configuration:
-    relative_path = ".."+os.sep+"config"+os.sep+"test_pid_gains.yaml"
+
+    relative_path = ".."+os.sep+".."+os.sep+"config"+os.sep+"test_pid_gains.yaml"
 
 
 
@@ -25,12 +30,21 @@ class Config_file_configuration:
 @param configuration object with "kp", "kd" and "ki" attributes (as float)
 """
 class PID:
+
     def __init__(self,configuration):
         self._configuration = configuration
         self._integral = 0
+
+    """!
+    return dictionary of gains, keys: "kp", "kd" and "ki"
+    """
+    def get_gains(self):
+        return {"kp":self._configuration.kp,"kd":self._configuration.kp,"ki":self._configuration.ki}
+
     """ ! resert integral part of the PID """
     def reset_integral(self):
         self._integral = 0
+
     """!
      compute the force related to the pid controller. 
      \warning this function is not stateless, as it performs integration. call reset_pid() to reset the integral part. 
@@ -44,8 +58,10 @@ class PID:
         position_error = position_target - position
         self._integral += delta_time * position_error
         return position_error*self._configuration.kp-velocity*self._configuration.kd+self._integral*self._configuration.ki
+
     def __str__(self):
         return "PID controller: kp:"+str(self._configuration.kp)+" kd:"+str(self._configuration.kd)+" ki:"+str(self._configuration.ki)
+
 
 
 ### convenience function for reading pid configuration from yaml file
@@ -54,18 +70,20 @@ def _read_yaml_config_file(file_path):
     # importing yaml and reading yaml file
     import yaml
     with open(file_path,"r") as f:
-        config = yaml.load(f)
+        config_dict = yaml.load(f)
     # checking the yaml file had the excepted entries
     expected_attributes = ["kp","kd","ki"]
     for attribute in expected_attributes:
-        if not hasattr(config,attribute):
-            raise Exception("Configuration file "+str(file_path)+" is excepted to have the "+str(attribute)+" entry")
-    # converting gains from str to floats
-    for attribute in excepted_attributes:
+        if not attribute in config_dict.keys():
+            raise Exception("Configuration file "+str(file_path)+" is expected to have the "+str(attribute)+" entry")
+    # creating a config object with correct attributes
+    class config(object): pass
+    c = config();
+    for attribute in expected_attributes:
         try : 
-            setattr(config,attribute,float(getattr(config,attribute)))
+            setattr(config,attribute,float(config_dict[attribute]))
         except Exception as e: 
-            raise Exception("failed to convert "+attribute+"("+str(getattr(config,attribute))+") to float (file: "+str(file_path)+")")
+            raise Exception("failed to convert "+attribute+"("+str(config_dict[attribute])+") to float (file: "+str(file_path)+")")
     # constructing and returning controller
     return PID(config)
 
@@ -118,21 +136,24 @@ def get_ros_params_pid(verbose=True):
 return PID based on gains read from default configuration file 
 Path to configuration file relative to the script where this function is 
 defined is specified in the Config_file_configuration object.
+@param config_file_path if None, uses default config file (in config folder), else used specified path
 @param verbose if True, print path to config file used to standard output
 @see PID
 @see Config_file_configuration
 """
-def get_config_file_pid(verbose=True):
-    # getting abs path to this script
-    abs_path_script = os.path.realpath(__file__)
-    # getting name of this file
-    script = os.basename(abs_path_script)
-    # getting abs path of folder in which this script is
-    abs_path =  abs_path_script[:-len(script)]
-    # getting abs path to config file
-    abs_path_config = abs_path+os.sep+Config_file_configuration.relative_path
+def get_config_file_pid(config_file_path=None,verbose=True):
+    if config_file_path is None:
+        # getting abs path to this script
+        abs_path_script = os.path.realpath(__file__)
+        # getting name of this file
+        script = os.path.basename(abs_path_script)
+        # getting abs path of folder in which this script is
+        abs_path =  abs_path_script[:-len(script)]
+        # getting abs path to config file
+        abs_path_config = os.path.abspath(abs_path+os.sep+Config_file_configuration.relative_path)
+    else : abs_path_config = config_file_path
     # checking file exists
-    if not os.isfile(abs_path_config):
+    if not os.path.isfile(abs_path_config):
         raise Exception("failed to find configuration file: "+str(abs_path_config))
     # printing path to config file if asked
     if verbose:
---
+++ ci_example_python/test/test_pid.py
@@ -0,0 +1,95 @@
+import unittest
+import yaml
+import os
+import ci_example.pid as PID
+
+
+# see CMakeLists.txt in root folder to see how to activate these tests
+# for continuous integration
+
+
+class PID_TESTCASE(unittest.TestCase):
+
+    YAML_CONFIG_FILE = "pid_config_test.yaml"
+
+    # testing the function get_config_file_pid will require a yaml config file
+    # creating this file here, which will be called before running all tests
+    def setUp(self):
+        with open(self.YAML_CONFIG_FILE,"w+") as f:
+            f.write(yaml.dump(PID.Default_configuration))
+    
+    # deleting the file created above when we leave the tests
+    def tearDown(self):
+        os.remove(self.YAML_CONFIG_FILE)
+
+    # testing creating a pid controller from file works as expected
+    def test_config_file_factory(self):
+        pid = PID.get_config_file_pid(config_file_path=self.YAML_CONFIG_FILE,verbose=False)
+        gains = pid.get_gains()
+        self.assertEqual(gains["kp"],PID.Default_configuration.kp)
+        self.assertEqual(gains["kd"],PID.Default_configuration.kd)
+        self.assertEqual(gains["ki"],PID.Default_configuration.ki)
+
+    # testing creating a pid controller from default config file 
+    def test_config_file_factory(self):
+        pid = PID.get_config_file_pid(verbose=False)
+   
+    # testing creation using default config
+    def test_default_factory(self):
+        pid = PID.get_default_pid()
+        gains = pid.get_gains()
+        self.assertEqual(gains["kp"],PID.Default_configuration.kp)
+        self.assertEqual(gains["kd"],PID.Default_configuration.kd)
+        self.assertEqual(gains["ki"],PID.Default_configuration.ki)
+
+    # testing creating a pid controller from a non existing file raises an exception
+    def test_exception_on_non_existing_config_file(self):
+        with self.assertRaises(Exception):
+            pid = PID.get_config_file_pid(config_file_path="non_existing_path",verbose=False)
+        
+    # testing integral integrates, except if ki is zero
+    def test_integral(self):
+        class config:
+            kp,kd,ki = 1,1,1
+        position = 1
+        velocity = 1
+        position_target = 2
+        delta_time = 0.1
+        pid = PID.PID(config)
+        force_1 = pid.compute(position,velocity,position_target,delta_time)
+        force_2 = pid.compute(position,velocity,position_target,delta_time)
+        self.assertNotEqual(force_1,force_2)
+        config.ki = 0
+        force_3 = pid.compute(position,velocity,position_target,delta_time)
+        force_4 = pid.compute(position,velocity,position_target,delta_time)
+        self.assertEqual(force_3,force_4)
+
+
+    # testing force is zero if already at target
+        class config:
+            kp,kd,ki = 1,1,1
+        position = 1
+        velocity = 0
+        position_target = 1
+        delta_time = 0.1
+        pid = PID.PID(config)
+        force = pid.compute(position,velocity,position_target,delta_time)
+        self.assertEqual(force,0)
+
+    # testing the controller pushes in the right direction
+        class config:
+            kp,kd,ki = 1,1,0
+        position = 0
+        velocity = 0
+        target_position = 1
+        delta_time = 0.1
+        pid = PID.PID(config)
+        force = pid.compute(position,velocity,target_position,delta_time)
+        self.assertTrue(force>0)
+        target_position = -1
+        force = pid.compute(position,velocity,target_position,delta_time)
+        self.assertTrue(force<0)
+        target_position = 0
+        velocity=1.0
+        force = pid.compute(position,velocity,target_position,delta_time)
+        self.assertTrue(force<0)
