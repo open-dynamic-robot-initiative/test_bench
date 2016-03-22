34513fb63ccd5b23171ce6c116e89c8e09b09c31
Vincent Berenz <vincentberenz@tuebingen.mpg.de>
2016-03-22 11:00:14 +0100

    deleted doc

--- ci_example_cpp/doc/html/search/all_64.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['default_5fconfiguration',['Default_configuration',['../classci__example_1_1Default__configuration.html',1,'ci_example']]]
-];
--- ci_example_cpp/doc/html/search/classes_67.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['gains_5fconfiguration',['Gains_configuration',['../classci__example_1_1Gains__configuration.html',1,'ci_example']]]
-];
