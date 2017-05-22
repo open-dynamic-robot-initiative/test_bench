365b0edbcb3d056fa76bf747afb7c61c53c21dd0
Vincent Berenz <vincentberenz@gmail.com>
2017-05-22 12:16:37 +0200

    doxygen with cross dependancies working fine

--- ci_doxygen_child_cpp/include/ci_doxygen_child/doxygen_child.h
+++ ci_doxygen_child_cpp/include/ci_doxygen_child/doxygen_child.h
@@ -4,11 +4,20 @@
 
 namespace ci_doxygen_child {
 
-  /*! example of a function referring to ci_example API.
-    the documentation of this function should link with success to 
-    ci_example::PID. See CMakeLists.txt for details
-   */
-  void dummy_function_using_ci_example_api (ci_example::PID &pid);
+  /* dummy class using the ci_example API */
+  class Dummy_class {
+
+  public:
+
+    /** 
+     * example of a function referring to ci_example API.
+     * the documentation of this function should link with success to 
+     * ci_example::PID. See CMakeLists.txt for details
+     * @param pid PID controller
+     */
+    void dummy_function_using_ci_example_api (ci_example::PID &pid);
+
+  };
 
 }
 
--- ci_doxygen_child_cpp/src/doxygen_child.cpp
+++ ci_doxygen_child_cpp/src/doxygen_child.cpp
@@ -2,7 +2,7 @@
 
 namespace ci_doxygen_child {
 
-  void dummy_function_using_ci_example_api (ci_example::PID &pid){}
+  void Dummy_class::dummy_function_using_ci_example_api (ci_example::PID &pid){}
 
 }
 
