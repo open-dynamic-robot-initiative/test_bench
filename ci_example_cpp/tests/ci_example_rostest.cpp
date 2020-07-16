75d1fdd9b4dede04939daefe9b37e0ae081b3d60
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-07-16 13:57:46 +0200

    Fix typo "Gesellshaft" --> "Gesellschaft"

--- ci_example_cpp/tests/ci_example_rostest.cpp
+++ ci_example_cpp/tests/ci_example_rostest.cpp
@@ -2,7 +2,7 @@
  * @file ci_example_rostest.cpp
  * @author Vincent Berenz
  * license License BSD-3-Clause
- * @copyright Copyright (c) 2019, New York University and Max Planck Gesellshaft.
+ * @copyright Copyright (c) 2019, New York University and Max Planck Gesellschaft.
  * @date 2019-05-22
  * 
  * @brief example of unit tests that use ROS
